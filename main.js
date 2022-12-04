3f79626a72f080abe513db58a402740d12fe745c
lirapanci <85112446+lirapanci@users.noreply.github.com>
2022-12-04 20:24:35 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+addEventListener('DOMContentLoaded', (event) => {
+
+    document.getElementById("bill-amount").addEventListener("keyup", () =>
+        calculate()
+    );
+
+    document.getElementById("people-no").addEventListener("keyup", () =>
+        calculate()
+    );
+
+    function clearClass() {
+        const activeClass = document.querySelector(".tipPercentage.active")
+        if (activeClass) {
+            activeClass.classList.remove("active");
+        }
+    }
+
+    const tipButtons = document.getElementsByClassName("tipPercentage")
+        for (var i = 0; i < tipButtons.length; i++){
+            tipButtons[i].addEventListener("click", (event) => {
+                clearClass();
+                event.target.classList.add("active");
+                calculate()
+            })
+        }
+
+    document.getElementById("custom-tip").addEventListener("keyup", () => {
+        const removeActive = document.getElementsByClassName("tipPercentage");
+        for(i = 0; i < removeActive.length; i++){
+            removeActive[i].classList.remove("active")
+        }
+        calculate()
+
+    });
+
+    document.getElementById("reset").addEventListener("click", () => {
+        const inputs = document.getElementsByTagName("input");
+        for(i = 0; i < inputs.length; i++){
+            inputs[i].value = ""
+        }
+
+        const removeActive = document.getElementsByClassName("tipPercentage");
+        for(i = 0; i < removeActive.length; i++){
+            removeActive[i].classList.remove("active")
+        }
+
+        calculate()
+
+    })
+
+});
+
+
+function calculate () {
+    document.getElementById("tip-amount-person").innerText = "0";
+    document.getElementById("total-amount-person").innerText = "0";
+
+    const billAmount = parseFloat(document.getElementById("bill-amount").value);
+
+    if (isNaN(billAmount)) return;
+
+    const tip = document.querySelector(".tipPercentage.active");
+    const customPercentage = parseInt(document.getElementById("custom-tip").value);
+
+    if (tip == null && customPercentage == 0) {
+       return;
+    }
+
+    let tipPercentage = 0;
+
+    if(customPercentage){
+        tipPercentage = customPercentage;
+    } else if(tip != null) {
+        tipPercentage = tip.dataset.percentage;
+    }
+
+    if(tipPercentage <= 0) return;
+
+    const nrPeople = parseInt(document.getElementById('people-no').value);
+
+    if (isNaN(nrPeople)) return;
+
+    const calculatedTip = calculateTip(billAmount, tipPercentage, nrPeople);
+
+    document.getElementById("tip-amount-person").innerText = calculatedTip.tip;
+    document.getElementById("total-amount-person").innerText = calculatedTip.total;
+}
+
+function calculateTip (billAmount, tipPercentage, nrPeople) {
+
+    const tipAmountPerson = tipPercentage / 100 * billAmount / nrPeople;
+    const totalAmountPerson = billAmount / nrPeople + tipAmountPerson;
+    const calculationValues = {
+        tip: Math.round(tipAmountPerson),
+        total: Math.round(totalAmountPerson)
+    };
+    return calculationValues;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'light-green': '#F3F9FA',
+      'green':'#9EBBBD',
+      'dark-green': '#00474B',
+      'selected-green': '#26C2AE',
+      'gray-greenish': '#5E7A7D',
+      'greenish': '#C5E4E7',
+      'white': '#FFFFFF',
+      'green-mint':'#9FE8DF',
+      'grayish-green': '#7F9D9F',
+      'gem-green': '#0D686D',
+      'custom-green': '#547878',
+    },
+    fontFamily:{
+      'mono': ['Space Mono', 'monospace']
+    },
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
